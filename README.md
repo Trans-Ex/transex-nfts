cdddf8c4178507a5c328587e2aea95965a0e78ba
jpw <user@user.com>
2024-02-20 16:26:11 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Trans.Ex NFT collection
+
+## Build
+```sh
+# build the debug version
+yarn build:debug
+# or
+yarn exec koinos-sdk-as-cli build-all debug 0 colections.proto 
+
+# build the release version
+yarn build:release
+# or
+yarn exec koinos-sdk-as-cli build-all release 0 colections.proto 
+```
+
+## Test
+```sh
+yarn test
+# or
+yarn exec koinos-sdk-as-cli run-tests
+```
---
+++ assembly/Constants.ts
@@ -0,0 +1,15 @@
+import { Base58 } from "@koinos/sdk-as";
+
+export namespace Constants {
+  export const NAME: string = "Trans.Ex Collection";
+  export const SYMBOL: string = "TRX";
+  export const MINT_PRICE: u64 = 8900000000;
+  export const MINT_FEE: bool = true;
+  export const MAX_SUPPLY: u64 = 111;
+  export const URI: string = "https://salmon-negative-koi-521.mypinata.cloud/ipfs/QmbnEz26G1Bstha3LiBfxC8cu6UH7XEGQVsXz85zAJFwCC";
+  export const OWNER: Uint8Array = Base58.decode("14dpibwJrMJUDBMCPVrbGrz3P2iX52ecDh");
+
+  // token mint
+  export const TOKEN_PAY: Uint8Array = Base58.decode("15DJN4a8SgrbGhhGksSBASiSYjGnMU8dGL");
+  export const ADDRESS_PAY: Uint8Array = Base58.decode("14dpibwJrMJUDBMCPVrbGrz3P2iX52ecDh");
+}
