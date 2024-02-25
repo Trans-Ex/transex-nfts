1b8301bfe2af3c52d0973e02c5514493893d0985
jpw <user@user.com>
2024-02-25 16:40:20 -0500

    update URI

--- assembly/Constants.ts
+++ assembly/Constants.ts
@@ -6,7 +6,7 @@ export namespace Constants {
   export const MINT_PRICE: u64 = 8588500000;
   export const MINT_FEE: bool = true;
   export const MAX_SUPPLY: u64 = 111;
-  export const URI: string = "https://salmon-negative-koi-521.mypinata.cloud/ipfs/QmbnEz26G1Bstha3LiBfxC8cu6UH7XEGQVsXz85zAJFwCC";
+  export const URI: string = "https://salmon-negative-koi-521.mypinata.cloud/ipfs/QmSGTypK3mVm8hSxd2eBA9LziZ3r7iJ8iamyTNag6wpLsK";
   export const OWNER: Uint8Array = Base58.decode("1CXSeGtcUTXH6pSCCF5FpPC5HgYJSbJEvs");
 
   // token mint
